5bc49f5723c2c2e6f2b2c0f17c1578ea33606f92
Joachim Reiss <reiss@bryter.io>
2024-04-02 19:40:46 +0200

    remove firenvim toggles

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,7 +2,6 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
-	lazy = not vim.g.started_by_firenvim,
 	config = function()
 		vim.opt.termguicolors = true
 		require("bufferline").setup({
