7c45eb286b4f6748992d16d98e5bdd88fda2bc11
Joachim Reiss <reiss@bryter.io>
2025-02-20 14:40:10 +0100

    change theme based on dark or light mode macos

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,8 +1,28 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("catppuccin")
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		local function get_macos_appearance()
+			local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+			if not handle then
+				return "light"
+			end
+			local result = handle:read("*a")
+			handle:close()
+			if result:match("Dark") then
+				return "dark"
+			else
+				return "light"
+			end
+		end
+
+		local appearance = get_macos_appearance()
+		if appearance == "dark" then
+			vim.cmd("colorscheme catppuccin-mocha")
+		else
+			vim.cmd("colorscheme catppuccin-latte")
+		end
+		-- vim.cmd.colorscheme("catppuccin")
+	end,
 }
