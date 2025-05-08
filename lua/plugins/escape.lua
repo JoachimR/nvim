2aaae5b0a775fbad05b82f262d7bacaee1065229
Joachim Reiss <reiss@bryter.io>
2025-05-08 18:43:41 +0200

    fix better escape usage, new version

--- lua/plugins/escape.lua
+++ lua/plugins/escape.lua
@@ -3,7 +3,21 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("better_escape").setup({
-			mapping = { "bb", "jj", "kk" },
+			timeout = vim.o.timeoutlen, -- after `timeout` passes, you can press the escape key and the plugin will ignore it
+			default_mappings = false,
+			mappings = {
+				i = {
+					k = {
+						k = "<Esc>",
+					},
+					j = {
+						j = "<Esc>",
+					},
+					b = {
+						b = "<Esc>",
+					},
+				},
+			},
 		})
 	end,
 }
