3cd32eb98b76ec8fdd4b1123ee1455b965f0ea37
Joachim Reiss <reiss@bryter.io>
2026-05-20 16:00:43 +0200

    migrate to vue_ls and clean up LSP config
    - Replace volar/ts_ls with vue_ls (Vue's new official LSP)
    - Use vim.lsp.config + vim.lsp.enable API
    - Add pnpm-aware TypeScript lib path resolution
    - Move ESLint autofix into LspAttach to scope per-client
    - Remove typescript-tools plugin and unused none-ls hooks

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,35 +6,88 @@ return {
 		end,
 	},
 	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"vue_ls",
+					"eslint",
+				},
+			})
+		end,
+	},
+	{
 		"neovim/nvim-lspconfig",
-		config = function(on_attach)
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+			-- Helper to find TypeScript lib (supports pnpm monorepos)
+			local function get_typescript_lib_path()
+				local cwd = vim.fn.getcwd()
+				-- Try standard node_modules path first
+				local standard_path = cwd .. "/node_modules/typescript/lib"
+				if vim.fn.isdirectory(standard_path) == 1 then
+					return standard_path
+				end
+				-- Try pnpm store (glob for versioned typescript)
+				local pnpm_glob = cwd .. "/node_modules/.pnpm/typescript@*/node_modules/typescript/lib"
+				local pnpm_matches = vim.fn.glob(pnpm_glob, false, true)
+				if #pnpm_matches > 0 then
+					return pnpm_matches[1]
+				end
+				-- Fallback to standard path (let LSP handle missing)
+				return standard_path
+			end
+
+			-- Lua
 			vim.lsp.config("lua_ls", {
 				capabilities = capabilities,
 			})
-			vim.lsp.config("kotlin_language_server", {})
+
+			-- Kotlin
+			vim.lsp.config("kotlin_language_server", {
+				capabilities = capabilities,
+			})
+
+			-- Bash
 			vim.lsp.config("bashls", {
 				capabilities = capabilities,
 			})
 
-      vim.lsp.config('eslint', {
-        capabilities = capabilities,
-        filetypes = { "vue", "typescript", "javascript" },
-        on_attach = function(_, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-          })
-        end,
-      })
+			-- Volar for Vue (also handles TypeScript in Vue projects)
+			vim.lsp.config("vue_ls", {
+				capabilities = capabilities,
+				filetypes = { "vue", "typescript", "javascript", "typescriptreact", "javascriptreact" },
+				init_options = {
+					vue = {
+						hybridMode = false,
+					},
+					typescript = {
+						tsdk = get_typescript_lib_path(),
+					},
+				},
+			})
 
+			-- ESLint with autofix on save
+			vim.lsp.config("eslint", {
+				capabilities = capabilities,
+				filetypes = { "vue", "typescript", "javascript", "typescriptreact", "javascriptreact" },
+			})
+
+			-- Enable all configured servers
+			vim.lsp.enable({ "lua_ls", "kotlin_language_server", "bashls", "vue_ls", "eslint" })
+
+			-- LSP Keymaps on attach
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
 					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
 					local opts = { buffer = ev.buf }
+					local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
 					vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts)
 					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
@@ -44,36 +97,16 @@ return {
 					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
 					vim.keymap.set("n", "<space>lr", vim.lsp.buf.rename, opts)
 					vim.keymap.set("n", "<space>la", vim.lsp.buf.code_action, opts)
+
+					-- ESLint autofix on save
+					if client and client.name == "eslint" then
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							buffer = ev.buf,
+							command = "EslintFixAll",
+						})
+					end
 				end,
 			})
-
-			vim.g.diagnostics_active = false
-			local function toggle_diagnostics()
-				if vim.g.diagnostics_active then
-					vim.g.diagnostics_active = false
-					vim.lsp.diagnostic.clear(0)
-					vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
-				else
-					vim.g.diagnostics_active = true
-					vim.lsp.handlers["textDocument/publishDiagnostics"] =
-						vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-							virtual_text = true,
-							signs = true,
-							underline = true,
-							update_in_insert = false,
-						})
-				end
-			end
-
-			vim.keymap.set("n", "<leader>dia", toggle_diagnostics, { noremap = true, silent = true })
 		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		-- config = function()
-		-- 	require("mason-lspconfig").setup({
-		-- 		ensure_installed = { "lua_ls" },
-		-- 	})
-		-- end,
 	},
 }
--- lua/plugins/typecheck.lua
+++ lua/plugins/typecheck.lua
@@ -1,7 +1,7 @@
 return {
 	"jellydn/typecheck.nvim",
 	dependencies = { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
-	ft = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact" },
+	ft = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact", "vue" },
 	opts = {
 		debug = true,
 		mode = "trouble", -- "quickfix" | "trouble"
